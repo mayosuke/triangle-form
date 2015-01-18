0c9e389d4c06c36c0e6dc36e7311363f331d96e8
Yosuke MIYAJIMA <mayo.suke@gmail.com>
2015-01-18 09:14:49 +0900

    Rename target file and test file.

--- triangle.rb
+++ triangle.rb
@@ -1,9 +1,10 @@
-class BonusDrink
-  def self.total_count_for(amount)
-    if amount - 3 >= 0
-      3 + total_count_for(amount - 3 + 1)
-    else
-      amount
-    end
+class Triangle
+  EQUILATERAL = 1 # 正三角形
+  ISOSCELES = 2 # 二等辺三角形
+  SCALENE = 3 # 不等辺三角形
+  NOT_TRIANGLE = 4 # 三角形でない
+
+  def self.detect_shape_for(edege1, edge2, edge3)
+    EQUILATERAL
   end
 end
