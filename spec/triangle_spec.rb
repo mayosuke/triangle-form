82b9b8f9f2b32d26a941735bb9bf3cb57378ddf6
Yosuke MIYAJIMA <mayo.suke@gmail.com>
2015-01-20 00:49:30 +0900

    Implement target code and its tests.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,4 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../triangle.rb')
 
 describe Triangle do
   specify { expect(Triangle.detect_shape_for(1,1,1)).to eq Triangle::EQUILATERAL }
+  specify { expect(Triangle.detect_shape_for(2,2,1)).to eq Triangle::ISOSCELES }
+  specify { expect(Triangle.detect_shape_for(2,3,4)).to eq Triangle::SCALENE }
+  specify { expect(Triangle.detect_shape_for(1,2,3)).to eq Triangle::NOT_TRIANGLE }
 end
+
--- triangle.rb
+++ triangle.rb
@@ -4,7 +4,41 @@ class Triangle
   SCALENE = 3 # 不等辺三角形
   NOT_TRIANGLE = 4 # 三角形でない
 
-  def self.detect_shape_for(edege1, edge2, edge3)
-    EQUILATERAL
+  def self.detect_shape_for(edge1, edge2, edge3)
+    if Triangle.is_triangle(edge1, edge2, edge3)
+      if Triangle.is_equilateral(edge1, edge2, edge3)
+        EQUILATERAL
+      elsif Triangle.is_isosceles(edge1, edge2, edge3)
+        ISOSCELES
+      else
+        SCALENE
+      end
+    else
+      NOT_TRIANGLE
+    end
+  end
+
+  def self.is_triangle(edge1, edge2, edge3)
+    if (edge1 < (edge2 + edge3)) && (edge2 < (edge1 + edge3)) && (edge3 < (edge1 + edge2))
+      true
+    else
+      false
+    end
+  end
+
+  def self.is_equilateral(edge1, edge2, edge3)
+    if (edge1 == edge2) && (edge2 == edge3)
+      true
+    else
+      false
+    end
+  end
+
+  def self.is_isosceles(edge1, edge2, edge3)
+    if (edge1 == edge2) || (edge2 == edge3) || (edge3 == edge1)
+      true
+    else
+      false
+    end
   end
 end
